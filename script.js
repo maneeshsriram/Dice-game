f51894d2f24a85c2b7e0f06cb55049b16004e30c
Maneesh <maneeshsriram123@gmail.com>
2020-10-31 23:41:25 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const num1 = Math.floor(Math.random() * 6) + 1
+const num2 = Math.floor(Math.random() * 6) + 1
+
+document.querySelector(".img1").setAttribute("src", `./images/dice${num1}.png`)
+document.querySelector(".img2").setAttribute("src", `./images/dice${num2}.png`)
+
+
+if (num1 > num2) {
+    document.querySelector("h1").innerText = "Player 1 wins"
+} else if (num2 > num1) {
+    document.querySelector("h1").innerText = "Player 2 wins"
+} else {
+    document.querySelector("h1").innerText = "Draw"
+}
